8fea0e2aaf26ff5e324cc880c20a38d71948f43a
Dekay <dkuehltau@protonmail.com>
2022-07-11 13:46:46 +0200

    Add project files.

---
+++ YetAnotherPasteTool/Plugin.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
+using System.IO;
+using Microsoft.Xna.Framework;
+
+namespace YetAnotherPasteTool
+{
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+        public override string Author => "Dekay";
+        public override string Description => "Tool for creating and pasting schematics";
+        public override string Name => "Test Plugin";
+        public override Version Version => new Version(1, 0);
+        public readonly string SCHEMATICS_PATH = "YetAnotherPasteTool";
+
+        public Plugin(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
+
+        private void OnInitialize(EventArgs args)
+        {
+            if (!Directory.Exists(Path.Combine(TShock.SavePath, SCHEMATICS_PATH)))
+            {
+                Directory.CreateDirectory(Path.Combine(TShock.SavePath, SCHEMATICS_PATH));
+            }
+
+            Commands.ChatCommands.Add(new Command("", SchematicCommand, "schematic", "sc"));
+        }
+
+        private void SchematicCommand(CommandArgs args)
+        {
+            if (args.Parameters.Count != 2)
+            {
+                args.Player.SendErrorMessage("Invalid syntax!");
+                args.Player.SendErrorMessage("Use /sc <save/paste/delete> name");
+            }
+
+            string subcommand = args.Parameters[0].ToLower();
+            string name = args.Parameters[1].ToLower();
+
+            switch (subcommand)
+            {
+                case "save":
+                    {
+                        // Creates a new schematic
+                        if (!args.Player.TempPoints.All(x => x.X + x.Y != 0))
+                        {
+                            args.Player.SendWarningMessage("You need to set a region to Save as schematic (/region set <1/2>)");
+                            return;
+                        }
+
+                        Point p1 = args.Player.TempPoints[0];
+                        Point p2 = args.Player.TempPoints[1];
+                        Schematic sc = new Schematic(Math.Min(p1.X, p2.X), Math.Min(p1.Y, p2.Y), Math.Abs(p1.X - p2.X) + 1, Math.Abs(p1.Y - p2.Y) + 1);
+                        string filePath = Path.Combine(TShock.SavePath, SCHEMATICS_PATH, name);
+                        bool result = sc.Save(filePath);
+                        if (result)
+                        {
+                        args.Player.SendInfoMessage("Saved selected region as Schematic '" + name + "'");
+                        } else
+                        {
+                            args.Player.SendErrorMessage("Error while saving schematic '" + name + "'");
+                        }
+                        break;
+                    }
+                case "paste":
+                    {
+                        // Pastes an existing schematic
+                        Point p1 = args.Player.TempPoints[0];
+                        if (!(p1.X + p1.Y != 0))
+                        {
+                            args.Player.SendWarningMessage("You need to set a upper left corner to paste the schematic (/region set 1)");
+                            return;
+                        }
+
+
+                        Schematic sc = Schematic.Load(Path.Combine(TShock.SavePath, SCHEMATICS_PATH, name));
+                        sc.Paste(p1.X, p1.Y);
+                        args.Player.SendInfoMessage("Schematic '" + name + "' successfully pasted");
+                        TSPlayer.All.SendTileRect((short)p1.X, (short)p1.Y, (byte)sc.Width, (byte)sc.Height);
+                        break;
+                    }
+                case "delete":
+                    {
+                        // Deletes an existing schematic
+                        string filePath = Path.Combine(TShock.SavePath, SCHEMATICS_PATH, name + Schematic.SCHEMATIC_FILE_ENDING);
+                        if (File.Exists(filePath))
+                        {
+                            File.Delete(filePath);
+                            args.Player.SendInfoMessage("Schematic '" + name + "' successfully deleted");
+                        }
+                        else
+                        {
+                            args.Player.SendErrorMessage("No schematic with that name exists");
+                        }
+                        break;
+                    }
+                default:
+                    args.Player.SendErrorMessage("Unknown subcommand specified: " + subcommand + "\nAvailable subcommands are: save, paste, delete");
+                    break;
+            }
+        }
+    }
+}
---
+++ YetAnotherPasteTool/Schematic.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Terraria;
+using System.IO;
+using OTAPI.Tile;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace YetAnotherPasteTool
+{
+    [Serializable]
+    public class Schematic
+    {
+        public readonly static string SCHEMATIC_FILE_ENDING = ".sc";
+
+        private readonly TileType[,] tileTypes;
+
+        public int Width
+        {
+            get
+            {
+                return tileTypes.GetLength(0);
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return tileTypes.GetLength(1);
+            }
+        }
+
+        public Schematic(int x, int y, int width, int height)
+        {
+            tileTypes = new TileType[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    TileType copy = new TileType();
+                    ITile tile = Main.tile[x + i, y + j];
+                    copy.Active = tile.active();
+                    copy.InActive = tile.inActive();
+                    copy.Type = tile.type;
+                    copy.Color = tile.color();
+                    copy.Slope = tile.slope();
+                    copy.Wall = tile.wall;
+                    copy.WallColor = tile.wallColor();
+                    copy.FrameX = tile.frameX;
+                    copy.FrameY = tile.frameY;
+                    copy.Liquid = tile.liquid;
+                    copy.LiquidType = tile.liquidType();
+                    copy.Actuator = tile.actuator();
+                    copy.Wire1 = tile.wire();
+                    copy.Wire2 = tile.wire2();
+                    copy.Wire3 = tile.wire3();
+                    copy.Wire4 = tile.wire4();
+                    tileTypes[i, j] = copy;
+                }
+            }
+        }
+
+        public void Paste(int x, int y)
+        {
+            for (int i = 0; i < tileTypes.GetLength(0); i++)
+            {
+                for (int j = 0; j < tileTypes.GetLength(1); j++)
+                {
+                    TileType scTile = tileTypes[i, j];
+                    Main.tile[x + i, y + j].active(scTile.Active);
+                    Main.tile[x + i, y + j].inActive(scTile.InActive);
+                    Main.tile[x + i, y + j].type = scTile.Type;
+                    Main.tile[x + i, y + j].color(scTile.Color);
+                    Main.tile[x + i, y + j].slope(scTile.Slope);
+                    Main.tile[x + i, y + j].wall = scTile.Wall;
+                    Main.tile[x + i, y + j].wallColor(scTile.WallColor);
+                    Main.tile[x + i, y + j].frameX = scTile.FrameX;
+                    Main.tile[x + i, y + j].frameY = scTile.FrameY;
+                    Main.tile[x + i, y + j].liquid = scTile.Liquid;
+                    Main.tile[x + i, y + j].liquidType(scTile.LiquidType);
+                    Main.tile[x + i, y + j].actuator(scTile.Actuator);
+                    Main.tile[x + i, y + j].wire(scTile.Wire1);
+                    Main.tile[x + i, y + j].wire2(scTile.Wire2);
+                    Main.tile[x + i, y + j].wire3(scTile.Wire3);
+                    Main.tile[x + i, y + j].wire4(scTile.Wire4);
+                }
+            }
+        }
+
+        public bool Save(string path)
+        {
+            string filename = path + SCHEMATIC_FILE_ENDING;
+            if (File.Exists(filename))
+            {
+                return false;
+            }
+
+            FileStream stream = File.Create(filename);
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, this);
+            stream.Close();
+
+            return true;
+        }
+
+        public static Schematic Load(string path)
+        {
+            string filename = path + SCHEMATIC_FILE_ENDING;
+            if (!File.Exists(filename))
+            {
+                return null;
+            }
+
+            FileStream stream = File.OpenRead(filename);
+            var formatter = new BinaryFormatter();
+            var v = (Schematic)formatter.Deserialize(stream);
+            stream.Close();
+
+            return v;
+        }
+    }
+
+    [Serializable]
+    internal struct TileType
+    {
+        internal bool Active { get; set; }
+        internal bool InActive { get; set; }
+        internal byte Liquid { get; set; }
+        internal bool Honey { get; set; }
+        internal bool Lava { get; set; }
+        internal bool Actuator { get; set; }
+        internal byte LiquidType { get; set; }
+        internal byte Slope { get; set; }
+        internal byte Color { get; set; }
+        internal ushort Type { get; set; }
+        internal bool Wire1 { get; set; }
+        internal bool Wire2 { get; set; }
+        internal bool Wire3 { get; set; }
+        internal bool Wire4 { get; set; }
+        internal ushort Wall { get; set; }
+        internal byte WallColor { get; set; }
+        internal short FrameX { get; set; }
+        internal short FrameY { get; set; }
+    }
+}
